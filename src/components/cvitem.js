bfebe09f6e568d4a20e19e462938e985dee73960
sez98 <sezal17101@iiitd.ac.in>
2021-07-27 21:55:28 +0530

    prop2 transfering class based to functional component

--- src/components/cvitem.js
+++ src/components/cvitem.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Item extends Component{
-    
+//We learn about transfering a attribute from one class component to functional component
 
-    state = {
-        keywords: ''
-    }
-
-    getInputHandler = (event) => {
-        this.setState({
-            input: event.target.value
-        })
-    }
-
-
-    render(){
-        
-        return (
-            <h4>
-                <input onChange= {this.getInputHandler} />            
-            </h4>
-        );
-        
-    }
+// the props here can have multiple attributes.
+//ex: <CVItem row = {row} key={row.id} name="sezal" age="22" address="srftrgR"/> etc.. 
+//hence instead of receiving all props and its properties.. we just recieve the one we want.
+//const CVItem = (props) => {
+//     return(
+//         <div>
+//             <h3>{props.row.id}. {props.row.title}</h3>
+//             <div>{props.row.feed}</div>
+//         </div>
+//     )
+// }
+const CVItem = ({row}) => {
+    return(
+        <div className="cvItem">
+            <h3>{row.id}. {row.title}</h3>
+            <div>{row.feed}</div>
+        </div>
+    )
 }
 
-export default Item;
+export default CVItem;
 
 
--- src/components/itemlist.js
+++ src/components/itemlist.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
+import CVItem from './cvitem';
+
+
 const ItemList = (props) => {
-         const itemsDB = props.DB1.map((row) => {
-                  return(
-                           <div key={row.id}>
-                                    <h3>{row.id}. {row.title}</h3>
-                                    <div>{row.feed}</div>
-                           </div>
-                  )
-         })
+         const itemsDB = props.DB1.map((row) => (
+                  <CVItem row={row} key={row.id}/>
+                  
+         ))
 
          return(
                   <>
                            <div>Welcome to the Items of the CV</div>
-                           {itemsDB}
+                           { itemsDB }
                   </>
          )
 }
