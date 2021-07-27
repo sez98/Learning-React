e3cd18488b9f12e06a9c2bca48dd84bb19283db8
sez98 <sezal17101@iiitd.ac.in>
2021-07-28 01:11:16 +0530

    react children + if-else styling + inline and classname based

--- src/components/header.js
+++ src/components/header.js
@@ -4,22 +4,43 @@ import React, { Component } from 'react';
 class Header extends Component{
 
     state= {
-        active: false
+        active: false,
+        keywords: '',
+        classname: 'header'
     }
 
     getYear() {
         var date = new Date();
         return date.getFullYear();
     }
+
+    inputHandler= (event) => {
+        // const value = event.target.value === '' ? false : true;
+        const value = event.target.value === '' ? 'header' : 'filledHeader';
+
+        this.setState({
+            active: value,
+            keywords: event.target.value,
+            classname: value
+        })
+    }
+
+    printValue() {
+        console.log(this.state.keywords);
+    }
+
+
     render(){        
         return (
             <>
-                <header className="header" style= {{background: `${this.state.active ? 'red' : 'green'}`}}>
+                {/* <header className="header" style= {{background: `${this.state.active ? 'yellow' : 'green'}`}}> */}
+                <header className={this.state.classname}>
                     <h1>Sezal Chug</h1>
                     <h2>Resume/CV</h2>
                     <h5>Last Updated in the Year {this.getYear()}</h5>
                 </header>
-                
+                <input onChange= {this.inputHandler}/>
+                <div><button onClick= {() => this.printValue()}> SUBMIT </button></div>
             </>
         );
     }
